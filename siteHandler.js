535a9765baae72b8ce248b8a1275e633c352acb8
Bofner <72773588+Bofner@users.noreply.github.com>
2023-02-23 18:33:59 +0900

    First github website trial

---
+++ siteHandler.js
@@ -0,0 +1,11 @@
+function trianglePuzzlePage(){
+    window.location='trianglePuzzle.html';
+}
+
+function homePage(){
+    window.location='index.html';
+}
+
+function bofnerPage(){
+    window.location='bofner.html';
+}
